71d9809a6f36b3c12a5384165d53aba3a523d1a0
Your Name <tranhoangloc502@gmail.com>
2023-02-22 14:00:25 +0700

    last change

--- App.js
+++ App.js
@@ -1,12 +1,187 @@
 import './App.css';
 import Demo from './demo'
+import {useEffect, useLayoutEffect, useReducer, useState} from "react";
+import {clear} from "@testing-library/user-event/dist/clear";
+//
+// const gifts = [
+//     'cpu',
+//     'memory',
+//     'card'
+// ]
+//
+// const render = () => (
+//     gifts[Math.floor(Math.random() * gifts.length)]
+// )
+//
+// function App() {
+//     const [state, setState] = useState('chưa có phần thưởng')
+//
+//
+//     return (
+//         // <Demo title ='linh'></Demo>
+//         <>
+//             <h1>{state}</h1>
+//             <button onClick={() => {
+//                 setState(render())
+//
+//             }}>click
+//             </button>
+//
+//
+//         </>
+//
+//     );
+// }
+
+
+// hiện todo list
+//
+// function App() {
+//
+//     const [state, setState] = useState([])
+//     const [state1, setState1] = useState('')
+//
+//
+//     const add = (user) => {
+//         setState(abc => [...abc, user])
+// }
+//
+//     return (
+//         <>
+//             {state.map(user => (
+//                 <ul>
+//                     <li>{user}</li>
+//                 </ul>
+//             ))}
+//
+//             <input type="text" value={state1} onChange={(event) => {
+//                setState1(event.target.value)
+//
+//             }}/>
+//             <button onClick={() => {
+//                 add(state1)
+//                 setState1('')
+//
+//             }}>Add
+//             </button>
+//         </>
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+
+// hook -useEffect with time function
+//
+// function App(){
+//     const [countdown,setCountdown] = useState(180)
+//     useEffect(()=>{
+//         setInterval(()=>{
+//             setCountdown(prev=>prev-1)
+//         },2000)
+//
+//
+//     },[])
+//
+//     return(
+//         <>
+//             <button  >click</button>
+//             <h1>{countdown}</h1>
+//         </>
+//     )
+//
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//up image
+// function App(){
+//     const [file,setFile] = useState('')
+// return(
+//     <>
+//         <input type="file"
+//                onChange={(event)=>{
+//                 const file =  event.target.files[0]
+//                    file.ok = URL.createObjectURL(file)
+//                    console.log(file.ok)
+//                    setFile(file)
+//                }}
+//         />
+//         <img src={file.ok}/>
+//
+//     </>
+// )
+// }
+
+
+
+
+
+
+
+
+
+ // todo list with reducer
+const initState= []
+const reducer = (state,action)=> {
+
+    switch (action.type) {
+        case 'ADD':
+            state = [action.payload, ...state];
+
+            return state
+
+        default:
+            return 'sai oke ?'
+
+    }
+}
 
 function App() {
-    return (
-        <Demo></Demo>
+    const [job, setJob] = useState('');
+    const handleOnChange = (e)=> {
+      setJob(e.target.value);
+    }
+
+    const add = ()=> {
+        dispatch({type: 'ADD', payload: job});
+        setJob('')
+    }
+    let [state,dispatch] = useReducer(reducer,initState);
+    return(
+        <>
+            <input  type="text" value={job} onChange={handleOnChange} />
+            <button  onClick={
+                add
 
 
-    );
+            } >ADD</button>
+            {state.map(job =>(
+                <h1>{job}</h1>
+                )
+
+            )}
+        </>
+    )
+
 }
 
 export default App;
--- demo.js
+++ demo.js
@@ -1,40 +1,70 @@
 import {Component, useState} from "react";
+//
+//
+export default function FComponent(props) {
+//     let [list,setList] = useState([
+//         {
+//             name: "loc",
+//             age:28
+//         },
+//         {
+//             name: "ngoc",
+//             age:28
+//         }
+//     ])
+//     let [name,setName] = useState('')
+//     let [age,setAge] = useState('')
+//     return(
+//         <>
+//             {list.map(item=>(
+//                 <h1>{item.name}-{item.age}</h1>
+//             ))}
+//
+//
+//             <input type="text" value={name} onChange={event => {
+//              setName(event.target.value)
+//             }}/>
+//             <input type="text" value={age} onChange={event => {
+//              setAge(event.target.value)
+//             }}/>
+//
+//
+//             <button onClick={()=>{
+//                 setList([...list,{name:name,age:age}])
+//                 setName('')
+//                 setAge('')
+//             }}>save</button>
+//
+//         </>
+//     )
+// }
+
+console.log(props)
+    const bro = [
+        {name: "javascript"},
+        {name: "php"},
+        {name: "python"}
+    ]
+
+// cách 1
+    let [a, b, c] = bro
+
+    const ul = <ol>
+        <li>{a.name}</li>
+        <li>{b.name}</li>
+        <li>{c.name}</li>
+    </ol>
 
 
-export default function FComponent(){
-    let [list,setList] = useState([
-        {
-            name: "loc",
-            age:28
-        },
-        {
-            name: "ngoc",
-            age:28
-        }
-    ])
-    let [name,setName] = useState('')
-    let [age,setAge] = useState('')
-    return(
-        <>
-            {list.map(item=>(
-                <h1>{item.name}-{item.age}</h1>
-            ))}
+// cách 2
 
+return (
+    <>
+        {ul}
+        {/*{bro.map(item =>*/}
+        {/*   <h1>{item.name}</h1>*/}
+        {/*)}*/}
 
-            <input type="text" value={name} onChange={event => {
-             setName(event.target.value)
-            }}/>
-            <input type="text" value={age} onChange={event => {
-             setAge(event.target.value)
-            }}/>
-
-
-            <button onClick={()=>{
-                setList([...list,{name:name,age:age}])
-                setName('')
-                setAge('')
-            }}>save</button>
-
-        </>
-    )
+    </>
+)
 }
